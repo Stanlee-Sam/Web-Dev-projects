33a6d6a29c0325c9b2ad2d6fcbd75ef74a14a3b8
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-10-28 11:37:36 +0300

    hero section

--- React/React + tailwind/tailwind react/src/components/Hero/Hero.jsx
+++ React/React + tailwind/tailwind react/src/components/Hero/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
   return (
     <div className="text-white">
       <div className="max-w-[800px] mt-[96px] h-screen w-full mx-auto text-center flex flex-col">
-        <p className="text-[#00df9a] font-bold p-2"> Growing with data analytics</p>
-        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6" >Grow with data</h1>
+        <p className="text-[#00df9a] font-bold pb-2"> Growing with data analytics</p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-4" >Grow with data</h1>
         <div className="flex justify-center items-center">
             <p className="md:text-5xl sm:text-4xl text-xl font-bold">Fast, flexible financing for</p>
-            <ReactTyped className="md:text-5xl sm:text-4xl text-xl font-bold pl-2"
+            <ReactTyped className="md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2"
                 strings={["BTC", "SASS", "BTB"]}
                 typeSpeed={50}
                 backSpeed={50}
@@ -16,6 +16,8 @@ const Hero = () => {
 
                 </ReactTyped>
         </div>
+        <p className="md:text-2xl text-xl text-gray-500">Monitor your data analytics to increase revenue for BTB, BTC & SASS platforms.</p>
+        <button className="bg-[#00df9a] w-[200px] rounded-sm font-medium my-6 py-2 mx-auto text-black">Get Started</button>
         
       </div>
     </div>
